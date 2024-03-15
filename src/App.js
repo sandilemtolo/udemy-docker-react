aea65e9a1e3cab4fbb3fb13cb3a6ab80aa4cf4a6
Sandile <sandile.mtolo5@gmail.com>
2024-03-15 17:38:09 +0200

    fronted: Update message

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          From sandile
+          From sandile, yay!!!
         </p>
         <a
           className="App-link"
